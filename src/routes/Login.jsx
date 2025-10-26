b4c9f054bdd65b1379588bd56eb7a618f7606eac
Rowel Timbal <practice_practice25@yahoo.com>
2025-10-26 16:38:39 -0400

    added sign in and create account page

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,7 +1,38 @@
+import { Container, Paper, Link, Avatar, Typography, Box, TextField, FormControlLabel, Checkbox, Button, Grid } from "@mui/material";
+import LockOutlineIcon from '@mui/icons-material/LockOutline';
+import {Link as ReactRouter} from 'react-router-dom'
+
 const Login = () => {   
+  const handleSubmit = () => {
+    console.log('Sign in submit')
+  }
   return (
   <>
-  <h1>This is Login Section</h1>
+  <Container maxWidth='xs'>
+    <Paper elevation={10} sx={{marginTop: 8, padding: 2}}>
+      <Avatar sx={{
+        mx: 'auto',
+        bgcolor: 'primary.main',
+        textAlign: 'center',
+        mb: 1
+      }}>
+        <LockOutlineIcon/>
+      </Avatar>
+      <Typography component='h1' variant="h5" sx={{textAlign:'center'}}>
+        Sign in
+      </Typography>
+      <Box component='form' onSubmit={handleSubmit} noValidate sx={{mt:1}}>
+        <TextField placeholder="Enter username" fullWidth required autoFocus sx={{mb:2}}></TextField>
+         <TextField placeholder="Enter password" fullWidth required type="password"  sx={{mb:2}}></TextField>
+         <FormControlLabel control={<Checkbox value='remember' color="primary"/>} label='Remember me'/>
+         <Button type="submit" variant="contained" fullWidth sx={{mt:1}}>SIGN IN</Button>
+         <Grid container justifyContent='space-between' sx={{mt:1}}>
+          <Link component={ReactRouter} to='/forgot'>Forgot password</Link>
+          <Link component={ReactRouter} to='/register'>Create account</Link>
+         </Grid>
+      </Box>
+    </Paper>
+  </Container>
   </>  
   )
 }
--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,5 +1,33 @@
-const Register = () => {            
-    return <h1>This is Register Section</h1>;
+import { Avatar, Container, Paper, Typography, Box, TextField, Button } from "@mui/material";
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+
+const Register = () => {       
+    const handleCreateAccountSubmit = () => {
+        alert('Create account submit')
+    }     
+    return (
+        <>
+            <Container maxWidth='xs'>
+                <Paper elevation={10} sx={{marginTop:8, padding:2}}>
+                    <Avatar sx={{
+                        mx:'auto',
+                        textAlign:'center',
+                        bgcolor:'primary.main',
+                        mb:1
+                    }}>
+                        <PersonAddIcon/>
+                    </Avatar>
+                    <Typography component='h1' variant="h5" textAlign='center' >Create Account</Typography>
+                    <Box component='form' onSubmit={handleCreateAccountSubmit} sx={{mt:1}}>
+                        <TextField placeholder="Enter username" fullWidth required autoFocus sx={{mb:2}}></TextField>
+                        <TextField placeholder="Enter password" type="password" fullWidth sx={{mb:2}}></TextField>
+                         <Button type="submit" variant="contained" fullWidth>Submit</Button>
+                    </Box>
+                   
+                </Paper>
+            </Container>
+        </>
+    )
 }
 
 export default Register;    
